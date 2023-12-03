501dad244b76d61ce408eef2531cfa0aa6d3ac80
Luis Felipe R . L <luisramires29032002@gmail.com>
2023-12-03 04:08:19 -0300

    configurando o ambiente

---
+++ src/main/java/com/ConsumindoApiJava/demoConsumindoApiJava/DemoConsumindoApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ConsumindoApiJava.demoConsumindoApiJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConsumindoApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConsumindoApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ConsumindoApiJava/demoConsumindoApiJava/DemoConsumindoApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ConsumindoApiJava.demoConsumindoApiJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConsumindoApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
